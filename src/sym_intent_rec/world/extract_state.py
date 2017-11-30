556fbdac9410e2cef598657ee126807746112210
Team5 <agrobomaniac@gmail.com>
2017-11-29 23:02:10 -0600

    Tune extract state

--- src/sym_intent_rec/world/extract_state.py
+++ src/sym_intent_rec/world/extract_state.py
@@ -6,6 +6,8 @@ from hlpr_perception_msgs.msg import SegClusters
 from tf import TransformListener
 import numpy as np
 import rospy
+import yaml
+import time
 import sys
 
 d_thresh = 0.06 # distance threshold
@@ -14,9 +16,9 @@ t_thresh = 0.5 # time threshold
 def check_present(i, t, t_seen, p_check, p_curr):
     near = np.linalg.norm(np.array(p_check) - p_curr, axis=1) < d_thresh
     seen = reduce(lambda x, y: x or y, near)
-    
+
     if not seen:
-        present = t - t_seen[i] > t_thresh
+        present = t - t_seen[i] < t_thresh
     else:
         present = True
         t_seen[i] = t
@@ -34,10 +36,10 @@ def publish_state(msg, args):
         p_curr.append(p)
 
     for i, p in enumerate(p_check['right']):
-        state.right[i] = check_present(i, t, t_seen['right'], p, p_curr)
-   
+        state.right.append(check_present(i, t, t_seen['right'], p, p_curr))
+    
     for i, p in enumerate(p_check['left']):
-        state.left[i] = check_present(i, t, t_seen['left'], p, p_curr)
+        state.left.append(check_present(i, t, t_seen['left'], p, p_curr))
 
     pub.publish(state)
 
@@ -49,6 +51,8 @@ if __name__ == "__main__":
     rospy.init_node('extract_state')
 
     tf = TransformListener(True, rospy.Duration(50))
+    time.sleep(5)
+
     pub = rospy.Publisher('/world_state', WorldState, queue_size=10)
     rospy.Subscriber('/beliefs/clusters', SegClusters, publish_state, (pub, tf, p_check, t_seen))
 
