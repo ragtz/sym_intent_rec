ef065961242cc238a172bad5c122f54c8ffea7ef
ragtz <agrobomaniac@gmail.com>
2017-12-02 10:14:12 -0600

    Swap positions of objects to correspond with cup positions given by visual system

--- src/sym_intent_rec/motion/playback_test.py
+++ src/sym_intent_rec/motion/playback_test.py
@@ -5,15 +5,6 @@ import pickle
 import time
 import sys
 
-def play_seq(manip_seq, gripper_pub, objs, obj_idx, bin_name): 
-    seq = manip_seq[objs][obj_idx][bin_name]
-    for action in seq:
-        if action['type'] == 'arm':
-            move_arm(action['msg'])
-        else:
-            move_gripper(gripper_pub, action['msg'])
-            time.sleep(3)
-
 if __name__ == "__main__":
     manip_seq = pickle.load(open(sys.argv[1]))
     objs = sys.argv[2]
