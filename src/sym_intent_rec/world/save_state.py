5aadff7ea51cdf5def5c93c1bab4cf5b78a5dfa6
irmt <agrobomaniac@gmail.com>
2018-02-12 21:33:44 -0600

    Change save state based on blob tracking

--- src/sym_intent_rec/world/save_state.py
+++ src/sym_intent_rec/world/save_state.py
@@ -1,32 +1,23 @@
 #!/usr/bin/env python
 
-from sym_intent_rec.world.utils import *
-from hlpr_perception_msgs.msg import SegClusters
-from tf import TransformListener
+from sensor_msgs.msg import Image
+from sym_intent_rec.world.extract_state import *
 import rospy
-import time
 import yaml
 import sys
 
-# assumes base_link frame
-def get_left_right(positions):
-    left = filter(lambda p: p[1] > 0, positions)
-    right = filter(lambda p: p[1] < 0, positions)
+def get_left_right(positions, mid=0):
+    left = filter(lambda p: p[0] < mid, positions)
+    right = filter(lambda p: p[0] >= mid, positions)
     return left, right 
 
 def save(msg, args):
-    tf = args[0]
-    filename = args[1]
-    positions = []
+    cups_pos, filename = args
 
-    for pc in msg.clusters:
-        position = get_avg_position(pc, tf)
-        positions.append(position)
+    positions = cups_pos(msg)
+    left, right = get_left_right(positions, msg.width/2.)
 
-    left, right = get_left_right(positions)
-
-    data = {'frame': '/base_link',
-            'left': left,
+    data = {'left': left,
             'right': right}
 
     yaml.dump(data, open(filename, 'w'))
@@ -39,10 +30,10 @@ if __name__ == "__main__":
     filename = sys.argv[1]
 
     rospy.init_node('save_state')
-    cluster_topic = '/beliefs/clusters'
-    tf = TransformListener(True, rospy.Duration(50))
+    image_topic = '/kinect/qhd/image_color'
 
-    time.sleep(5)
-    msg = rospy.wait_for_message(cluster_topic, SegClusters, timeout=5)
-    save(msg, (tf, filename))
+    cups_pos = get_cups(h_range, s_range, v_range)
+
+    msg = rospy.wait_for_message(image_topic, Image, timeout=5)
+    save(msg, (cups_pos, filename))
 
