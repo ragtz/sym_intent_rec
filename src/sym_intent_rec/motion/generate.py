ccdce76492270708d85ead5b6782e1e75967eea0
ragtz <agrobomaniac@gmail.com>
2018-02-13 00:41:46 -0600

    Add undo pour

--- src/sym_intent_rec/motion/generate.py
+++ src/sym_intent_rec/motion/generate.py
@@ -130,7 +130,12 @@ def pour(start, goal_bin_idx, n_wp=2):
             g[-1] += step
         wp.append(g)
 
-    joint_traj = array_to_joint_traj([start['joints']] + wp)
+    wp_rev = deepcopy(wp)[:-1]
+    wp_rev.reverse()
+
+    wp.extend(wp_rev)
+
+    joint_traj = array_to_joint_traj([start['joints']] + wp + [start['joint']])
     return {'type': 'arm', 'msg': joint_traj}, start
 
 def generate_seq(start, obj, bins, goal_bin_idx, m_type='leg'):
